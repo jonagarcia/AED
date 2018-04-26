d05ba5f5adb80179c60180c1d1baddcd3967a607
jonagarcia <38016252+jonagarcia@users.noreply.github.com>
2018-04-26 18:35:43 -0300

    Add files via upload

---
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cassert>
+#include <string>
+
+
+main()
+{
+ //declaraciones:	
+ int a=5;
+ int b=4;
+ int c=1;
+ char d= 'D';
+ bool e= true;
+ bool f= false;
+ double g= 10.01;
+ unsigned h= 10000;
+ 
+ //condiciones para compilacion: 
+ assert(a+b+c==10);
+ assert(e!=f);
+ assert(g>10);
+ assert(g<h);
+}
+
+  
+	
+	
+	
+
