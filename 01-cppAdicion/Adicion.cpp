15fc18734d2f618ad81b0566e342d05044896a96
jonagarcia <38016252+jonagarcia@users.noreply.github.com>
2018-04-26 13:52:21 -0300

    Add files via upload

---
+++ 01-cppAdicion/Adicion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdlib.h>
+
+main()
+{
+	int n1,n2, suma;
+	std::cout<<"introdusca el primer numero: ";
+	std::cin>>n1;
+	std::cout<<"\nintrodusca el  segundo numero: ";
+	std::cin>>n2;
+	suma=n1+n2;
+	
+	std::cout<<"\n\nEl Resultado es: " <<suma;
+	
+		
+}
